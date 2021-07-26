0ff65a6044963fe2e164f5b30278a5dc8502b07e
Cointor.exe <71720065+Cointor@users.noreply.github.com>
2021-07-26 15:39:24 -0500

    Update Basura-img.js

--- Codes/Imágenes/Basura-img.js
+++ Codes/Imágenes/Basura-img.js
@@ -1,5 +1,8 @@
 // Un Comando de basura con el Avatar, Ejemplo: https://media.discordapp.net/attachments/850469128988000286/866475294335434793/basura.png?width=335&height=406
 
+client.on("message", message => {
+if(message.author.bot) return;
+if (message.content.startsWith("Comando")) { 
 const marsnpm = require("marsnpm"); 
 message.channel.startTyping();
 setTimeout(() => {
@@ -10,3 +13,5 @@ let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
 let imagen = await marsnpm.basura(avatar); 
 let basura = new Discord.MessageAttachment(imagen, "basura.png") 
 message.channel.send(basura);
+ }
+});
