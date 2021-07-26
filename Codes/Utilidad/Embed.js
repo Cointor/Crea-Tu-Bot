51884e188bfb7db9ce662b60bf48985e6e16d47a
Cointor.exe <71720065+Cointor@users.noreply.github.com>
2021-07-26 16:05:30 -0500

    Create Embed.js

---
+++ Codes/Utilidad/Embed.js
@@ -0,0 +1,16 @@
+ client.on("message", message => {
+ if(message.author.bot) return; 
+if (message.content.startsWith("Comando")) { 
+const embed= new Discord.MessageEmbed() 
+.setTitle("Titulo, Soporta 256 caracteres") 
+.setAuthor(message.author.username, message.author.displayAvatarURL()) 
+.setColor("#6064f4") // Aqui es un Color Hex, lo puedes cambiar. 
+.setDescription("Descripcion, Soporta 2048 caracteres.")
+.setFooter("Pie de pagina, Soporta 1024 caracteres", client.user.avatarURL()) 
+.setImage(message.author.displayAvatarURL()) 
+.setThumbnail(message.author.displayAvatarURL()) 
+.setTimestamp() .setURL("http://github.com/cointor/") 
+.addField("Este es un campo puedes añadir solo 25", "Esta es la descipcion") 
+ message.channel.send(embed); 
+ } 
+});
