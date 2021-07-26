a5b47ad9d94f9a09aa8b180ecbe0680d419a515e
Cointor.exe <71720065+Cointor@users.noreply.github.com>
2021-07-26 17:27:34 -0500

    Create Calculadora.js

---
+++ Codes/Utilidad/Calculadora.js
@@ -0,0 +1,22 @@
+ client.on("message", message => {
+ if(message.author.bot) return; 
+if (message.content.startsWith("Comando")) { 
+const { MessageEmbed } = require("discord.js");
+const math = require("mathjs");
+    try {
+      if (!args[0]) return message.channel.send("!Por favor, dame una ecuación!");
+
+      const embed = new MessageEmbed()
+        .setColor("6064f4")
+        .setAuthor(`CALCULADORA`)
+        .addField(`Pregunta`,(args.join(" ")))
+        .addField(`Respuesta`,math.evaluate(args.join(" ")));
+
+      message.channel.send(embed);
+    } catch (error) {
+      message.channel.send(`Por favor, dame una ecuación válida
+`)
+    } 
+  
+} 
+});
