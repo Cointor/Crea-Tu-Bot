29bdeaa98c54b0cf66699b0ef35db1f04205563f
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:31:52 -0500

    Create Pixel-img.js

---
+++ Codes/Imágenes/Pixel-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 5000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.pixel(avatar); 
+let pixel = new Discord.MessageAttachment(imagen, "pixel.png") 
+message.channel.send(pixel);
+} 
+});
