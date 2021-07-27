c86ff54f813cea48b1e83874edb14e06cec565ae
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:22:55 -0500

    Create Eso-img.js

---
+++ Codes/Imágenes/Eso-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 5000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.eso(avatar); 
+let eso = new Discord.MessageAttachment(imagen, "eso.png") 
+message.channel.send(eso);
+} 
+});
