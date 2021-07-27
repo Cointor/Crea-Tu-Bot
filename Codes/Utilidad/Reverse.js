b59f353ae80cb4b02bb415be2374217e3c915be4
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:34:58 -0500

    Create Reverse.js

---
+++ Codes/Utilidad/Reverse.js
@@ -0,0 +1,13 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+if(!args[0]) return message.channel.send("Tienes que escribir un texto.");
+
+const embed = new Discord.MessageEmbed()
+.setTitle("Texto Alrevez")
+.setColor("#000")
+.setDescription(args.join(' ').split('').reverse().join(''), { allowedMentions: { parse: [] }})
+message.channel.send(embed);
+} 
+});
