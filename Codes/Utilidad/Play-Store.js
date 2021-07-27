71c5b80959b60e313e3d82e534070e5c496aecc0
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:32:47 -0500

    Create Play-Store.js

---
+++ Codes/Utilidad/Play-Store.js
@@ -0,0 +1,66 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const PlayStore = require("google-play-scraper");
+
+
+        if (!args[0])
+
+      return message.channel.send(
+
+        `¡Por favor, dé algo para buscar!`
+
+      );
+
+    PlayStore.search({
+
+      term: args.join(" "),
+
+      num: 1
+
+    }).then(Data => {
+
+      let App;
+
+      try {
+
+        App = JSON.parse(JSON.stringify(Data[0]));
+
+      } catch (error) {
+
+        return message.channel.send(
+
+          `No se encontró ninguna aplicación`
+
+        );
+
+      }
+
+      let Embed = new Discord.MessageEmbed()
+
+        .setColor("#6064f4")
+
+        .setThumbnail(App.icon)
+
+        .setURL(App.url)
+
+        .setTitle(`${App.title}`)
+
+        .setDescription(App.summary)
+
+        .addField(`Precio`, App.priceText, true)
+
+        .addField(`Creador`, App.developer, true)
+
+        .addField(`Clasificación`, `${App.scoreText}/5`, true)
+
+        .setFooter(`Pedido por ${message.author.username}`)
+
+        .setTimestamp();
+
+      return message.channel.send(Embed);
+
+    });
+} 
+});
