fc7c8ced2d78d6e49ef8a7ab34222b898ab58305
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:46:32 -0500

    Create Wanted.js

---
+++ Codes/Imágenes/Wanted.js
@@ -0,0 +1,15 @@
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 15000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.wanted(avatar); 
+let wanted = new Discord.MessageAttachment(imagen, "wanted.png") 
+message.channel.send(wanted);
+} 
+});
