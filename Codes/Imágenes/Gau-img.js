bd1cc2b6776c1c6060cb23b1380b6da5abf13f78
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:23:23 -0500

    Create Gau-img.js

---
+++ Codes/Imágenes/Gau-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 15000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.gaussian(avatar); 
+let gaussian = new Discord.MessageAttachment(imagen, "gaussian.png") 
+message.channel.send(gaussian);
+} 
+});
