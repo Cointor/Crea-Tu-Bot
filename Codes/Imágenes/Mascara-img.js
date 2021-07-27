ef0394628f59e96a802c7df06cb0e6eed8411617
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:27:14 -0500

    Create Mascara-img.js

---
+++ Codes/Imágenes/Mascara-img.js
@@ -0,0 +1,17 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 5000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.mascara(avatar); 
+let mascara = new Discord.MessageAttachment(imagen, "mascara.png") 
+message.channel.send(mascara);
+
+} 
+});
