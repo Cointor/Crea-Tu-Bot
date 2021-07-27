cdf95577faa5eec049ae4dfe55eace32b98df631
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:40:57 -0500

    Create Thisis-img.js

---
+++ Codes/Imágenes/Thisis-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 15000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.thisis(avatar); 
+let thisis = new Discord.MessageAttachment(imagen, "thisis.png") 
+message.channel.send(thisis);
+} 
+});
