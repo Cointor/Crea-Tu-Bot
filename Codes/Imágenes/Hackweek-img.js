88bb1bb1b16b167a0b7831dd5bfffa279c97869c
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:25:03 -0500

    Create Hackweek-img.js

---
+++ Codes/Imágenes/Hackweek-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 15000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.hackweek(avatar); 
+let hackweek = new Discord.MessageAttachment(imagen, "hackweek.png") 
+message.channel.send(hackweek);
+} 
+});
