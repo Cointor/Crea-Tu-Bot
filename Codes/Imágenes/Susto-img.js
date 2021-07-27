a79089034b0d3533b68eb554d5f4ccc940d5957f
Cointor <71720065+Cointor@users.noreply.github.com>
2021-07-27 06:40:03 -0500

    Create Susto-img.js

---
+++ Codes/Imágenes/Susto-img.js
@@ -0,0 +1,16 @@
+  
+ client.on("message", message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+const marsnpm = require("marsnpm"); 
+message.channel.startTyping();
+setTimeout(() => {
+  message.channel.stopTyping()
+}, 15000);
+let userm = message.mentions.users.first() || message.author; 
+let avatar = userm.displayAvatarURL({dynamic: false, format: 'png', size: 2048})
+let imagen = await marsnpm.susto(avatar); 
+let susto = new Discord.MessageAttachment(imagen, "susto.png") 
+message.channel.send(susto);
+} 
+});
