ed3a9e1dfe76501919fb6371f7e9ee24c1eb6a57
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-03 21:49:19 -0500

    Update Say.js

--- Codes/Utilidad/Say.js
+++ Codes/Utilidad/Say.js
@@ -3,14 +3,10 @@
  if (message.content.startsWith("Comando")) { 
  const args = message.content.trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-const { Util } = require('discord.js'); 
-if (!args) return message.reply(`Necesitas colocar algo que decir `);
-let mensaje = args.join(" "); 
-for(let i = 0; mensaje.includes("@here") || mensaje.includes("@everyone") || contenidos.includes("<@&870481988107259914>"); i++){
-    mensaje = mensaje.replace(/@here/g, "here");
-    mensaje = mensaje.replace(/@everyone/g, "everyone");
-    mensaje = mensaje.replace(/<@&870481988107259914>/g, "`ROL_NO_MENCIONABLE`");
-}
-return message.channel.send(Util.cleanContent(mensaje, message)).catch(e => e);
+
+  if (args.join(" ").length < 1) return message.channel.send("Debes escribir un mensaje a decir.");
+let permisos = message.channel.permissionsFor(message.member);
+message.delete()
+message.channel.send(args.join(" "), { disableMentions: permisos.has("MENTION_EVERYONE") ? "none" : "everyone"})
 } 
 });
