e26182a642ae06361d255971d3665d64e9d2fd0a
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-21 00:09:49 -0500

    Ya sirve

--- Codes/Utilidad/OffBot.js
+++ Codes/Utilidad/OffBot.js
@@ -1,6 +1,10 @@
  client.on("message", async message => {
  if(message.author.bot) return; 
  if (message.content.startsWith("Comando")) { 
-
+ if (message.author.id !== 'AQUI TU ID') {
+			return message.channel.send('¡No puede usar este comando!');
+		}
+		await message.channel.send('Apagando bot...');
+		return process.exit();
 } 
 });
