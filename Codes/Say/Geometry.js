80e7629a686a993ce48ed29b77b1fc9306ee5196
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-31 20:59:12 -0500

    Ya laja

--- Codes/Say/Geometry.js
+++ Codes/Say/Geometry.js
@@ -5,6 +5,8 @@
 setTimeout(() => {
   message.channel.stopTyping()
 }, 5000);
+  const args = message.content.trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
    if(!args[0]) return message.channel.send("Escribe el texto a copiar ") 
   let texto = args.join('%20');
   let attachment = new Discord.MessageAttachment(`https://gdcolon.com/tools/gdlogo/img/${texto}`, 'logo.png') 
