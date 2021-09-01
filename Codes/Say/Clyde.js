0f237a13772b791bd54f628de396d03a5bb2c15a
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-31 20:55:46 -0500

    Ya sirve xd

--- Codes/Say/Clyde.js
+++ Codes/Say/Clyde.js
@@ -2,6 +2,8 @@
  client.on("message", async message => {
  if(message.author.bot) return; 
  if (message.content.startsWith("Comando")) { 
+ const args = message.content.trim().split(/ +/g);
+ const command = args.shift().toLowerCase();
 if(!args[0]) return message.channel.send("Escribe el texto a copiar ") 
 let mensaje = args.join('%20');
 let attachment = new Discord.MessageAttachment
