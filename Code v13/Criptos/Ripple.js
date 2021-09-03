aae6d641a4d970c34462483d2634e73777657ba6
Cointor <71720065+Cointor@users.noreply.github.com>
2021-09-03 15:02:58 -0500

    Comando Ripple v13

---
+++ Code v13/Criptos/Ripple.js
@@ -0,0 +1,23 @@
+  client.on("messageCreate", async message => {
+ if(message.author.bot) return; 
+ if (message.content.startsWith("Comando")) { 
+   let price = require('crypto-price')
+  let Base = "USD"
+      let Crypto = "XRP"
+    price.getCryptoPrice(Base, Crypto).then(obj => { 
+        let precio = obj.price
+        var num = Number(precio);
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Precio Ripple")
+        .setDescription(`El precio de Ripple en dolares es de `+ `**$ ${num.toFixed(2)}**`)
+        .setTimestamp(new Date()) 
+        .setColor("#23292f")
+        .setFooter("Datos sobre la moneda brindados por CoinMarketCap")
+        .setThumbnail("https://media.discordapp.net/attachments/763118472573222992/806369648399613962/52.png")
+        message.channel.send({ embeds: [embed] });
+    }).catch(err => {
+        console.log(err)
+    })
+
+} 
+});
